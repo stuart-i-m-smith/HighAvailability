fe9028bcf98084c990d7a9adce7a6d65b6ddc1d7
stuart-i-m-smith <stuart.i.m.smith@gmail.com>
2022-03-17 12:18:05 +0000

    Working version.

---
+++ src/main/java/AvailabilityListener.java
@@ -0,0 +1,4 @@
+public interface AvailabilityListener {
+
+    void onMakeAvailable();
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,35 +1,52 @@
-import com.hazelcast.client.HazelcastClient;
-import com.hazelcast.client.config.ClientConfig;
 import com.hazelcast.cluster.MembershipEvent;
 import com.hazelcast.cluster.MembershipListener;
+import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.invoke.MethodHandles;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-public class Main implements MembershipListener {
+public class Main implements MembershipListener, AvailabilityListener {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
+    private HazelcastInstance instance = null;
+    private final List<AvailabilityListener> availabilityListeners = new ArrayList<>();
+    
     public static void main(String[] args) throws Exception {
-
         Main main = new Main();
         main.initialize();
-
     }
 
     public void initialize() throws Exception {
         LOGGER.info("Initializing");
 
-        ClientConfig config = ClientConfig.load();
-
-        HazelcastInstance instance = HazelcastClient.newHazelcastClient(config);
+        instance = Hazelcast.newHazelcastInstance();
         instance.getCluster().addMembershipListener(this);
+        addAvailabilityListener(this);
 
-        Thread.sleep((TimeUnit.SECONDS.toMillis(20)));
+        if(isPrimaryInstance()){
+            LOGGER.info("This is the primary instance.");
+            notifyAllListeners();
+        }
 
+        Thread.sleep((TimeUnit.SECONDS.toMillis(30)));
+    }
+
+    private boolean isPrimaryInstance() {
+        return instance.getCluster().getMembers().size() <= 1;
+    }
+
+    private void notifyAllListeners() {
+        availabilityListeners.forEach(AvailabilityListener::onMakeAvailable);
+    }
+
+    public void addAvailabilityListener(AvailabilityListener listener){
+        availabilityListeners.add(listener);
     }
 
     @Override
@@ -40,5 +57,15 @@ public class Main implements MembershipListener {
     @Override
     public void memberRemoved(MembershipEvent membershipEvent) {
         LOGGER.info("Member removed <{}>", membershipEvent);
+
+        if(isPrimaryInstance()){
+            LOGGER.info("Failover event detected <{}>", membershipEvent);
+            notifyAllListeners();
+        }
+    }
+
+    @Override
+    public void onMakeAvailable() {
+        LOGGER.info("Oh I better start doing stuff now!");
     }
 }
